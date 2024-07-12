fbb61f56a640a36a214561c5c784a2d789a0518a
Eugenia Minnah <eaminnah@st.ug.edu.gh>
2024-07-12 12:18:53 +0000

    Added code to my ProductDetail Screen

--- App/Screens/ProductDetailScreen.jsx
+++ App/Screens/ProductDetailScreen.jsx
@@ -1,19 +1,68 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useContext } from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
+import { CartContext } from '../Components/CartContext';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const ProductDetailScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <View>
-      <Text></Text>
-    </View>
-  </SafeAreaView>
-);
+export default function ProductDetailScreen({ route, navigation }) {
+    const { product } = route.params; // Get the product passed from HomeScreen
+    const { addToCart } = useContext(CartContext);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+                <Icon style={{ marginHorizontal: 10 }} name='arrow-left' size={30} onPress={() => navigation.goBack()} />
+                <Text style={styles.headerTitle}>Product Details</Text>
+            </View>
+            <ScrollView contentContainerStyle={styles.contentContainer}>
+                <Image source={{ uri: product.image }} style={styles.productImage} />
+                <Text style={styles.productTitle}>{product.title}</Text>
+                <Text style={styles.productDescription}>{product.description}</Text>
+                <Text style={styles.productPrice}>${product.price}</Text>
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 16,
+        paddingHorizontal: 16,
+    },
+    headerTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginLeft: 10,
+    },
+    contentContainer: {
+        padding: 16,
+    },
+    productImage: {
+        width: '100%',
+        height: 300,
+        resizeMode: 'contain',
+        marginBottom: 16,
+    },
+    productTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 8,
+    },
+    productDescription: {
+        fontSize: 16,
+        color: '#8c8c8c',
+        marginBottom: 16,
+    },
+    productPrice: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#ff6600',
+        marginBottom: 16,
+    },
+    
 });
-import { CartContext } from '../Components/CartContext';
-export default ProductDetailScreen;
